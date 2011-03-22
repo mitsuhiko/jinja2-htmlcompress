42fb85cae7162d25dafb5cd264fc4b692dedb8b0
Armin Ronacher <armin.ronacher@active-4.com>
2011-03-22 09:35:34 +0100

    Preserve script and style tags and a bunch of others

--- jinja2htmlcompress.py
+++ jinja2htmlcompress.py
@@ -12,20 +12,54 @@
 import re
 from jinja2.ext import Extension
 from jinja2.lexer import Token
+from jinja2 import TemplateSyntaxError
+
+
+_tag_re = re.compile(r'<(/?)([a-zA-Z0-9_-]+)')
 
 
 class HTMLCompress(Extension):
+    isolated_tags = frozenset(['script', 'style', 'pre', 'textarea'])
 
-    def normalize(self, value):
-        value = re.sub(r'\s*<', '<', value)
-        return re.sub(r'>\s*', '>', value)
+    def isolated(self, stack):
+        for tag in reversed(stack):
+            if tag in self.isolated_tags:
+                return True
+        return False
+
+    def normalize(self, token, stack, stream):
+        pos = 0
+        buffer = []
+        def write_data(value):
+            if not self.isolated(stack):
+                value = value.strip()
+            buffer.append(value)
+
+        for match in _tag_re.finditer(token.value):
+            closes, tag = match.groups()
+            preamble = token.value[pos:match.start()]
+            write_data(preamble)
+            buffer.append(match.group())
+            if closes:
+                if stack.pop() != tag:
+                    raise TemplateSyntaxError('HTML has to be balanced '
+                        'when htmlcompress extension is active',
+                        token.lineno, stream.name, stream.filename)
+            else:
+                stack.append(tag)
+            pos = match.end()
+
+        write_data(token.value[pos:])
+        return u''.join(buffer)
 
     def filter_stream(self, stream):
+        stack = []
         for token in stream:
             if token.type != 'data':
                 yield token
                 continue
-            yield Token(token.lineno, 'data', self.normalize(token.value))
+            value = self.normalize(token, stack, stream)
+            yield Token(token.lineno, 'data', value)
 
 
 def test():
@@ -36,6 +70,11 @@ def test():
           <head>
             <title>{{ title }}</title>
           </head>
+          <script type=text/javascript>
+            if (foo < 42) {
+              document.write('Foo < Bar');
+            }
+          </script>
           <body>
             <li><a href="{{ href }}">{{ title }}</a></li>
           </body>
